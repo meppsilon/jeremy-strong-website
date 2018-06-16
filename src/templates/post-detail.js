5459474f3365a20a8759fee7dcc55267ecefa5d6
mepps32 <mepps32@gmail.com>
2018-06-16 15:18:47 -0700

    Add posts for jeremy site

---
+++ src/templates/post-detail.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export const PostDetailTemplate = ({ title, image, description }) => (
+  <section className="section">
+    <div className="container content">
+      <div className="columns">
+        <div className="column is-10 is-offset-1">
+          <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+            {title}
+          </h1>
+          <p>{description}</p>
+          <img alt={description} src={image} />
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+const PostDetail = ({ data: { post } }) => (
+  <PostDetailTemplate
+    title={post.frontmatter.title}
+    description={post.frontmatter.description}
+    image={post.frontmatter.image}
+  />
+);
+
+export default PostDetail;
+
+export const postDetailQuery = graphql`
+  query PostDetail($id: String!) {
+    post: markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+        description
+        image
+      }
+    }
+  }
+`;
