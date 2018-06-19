b7a5a51dac69b25ee89018cdcdf32ddf1dbf54f6
Ammon <skatea09@aol.com>
2018-06-18 22:17:56 -0700

    adds responsiveness

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -1,91 +1,43 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import musicContent from "../content/music.js";
 
 const MusicSection = () => (
-  <div id="music">
-    <div className="bg-black-true">
-      <div className="text-white py-2 text-center music-title text-3xl">
+  <div id="music" className="bg-black-true">
+    <div className="sm:w-9/10 mx-auto">
+      <div className="text-white py-2 text-center music-title text-3xl sm:text-left md:text-center">
         Music
       </div>
-      <div className="sm:flex sm:flex-wrap">
-        <div className="text-white flex flex-col relative py-4 sm:w-2/5">
-          <div className="flex justify-center relative">
-            <div className="w-full h-full relative aspect-ratio-16/9">
-              <ReactPlayer
-                url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-                className="pin-t pin-l absolute"
-                width="100%"
-                height="100%"
-                controls
-              />
+      <div className="md:flex md:flex-wrap md:justify-between">
+        {musicContent.map(song => (
+          <div className="text-white flex flex-col relative py-4 sm:flex-row md:flex-col md:w-3/10">
+            <div className="flex justify-center relative sm:w-full">
+              <div className="w-full h-full relative aspect-ratio-16/9">
+                <ReactPlayer
+                  url={song.link}
+                  className="pin-t pin-l absolute"
+                  width="100%"
+                  height="100%"
+                  controls
+                />
+              </div>
+            </div>
+            <div className="w-2/3 pt-3 mx-auto sm:pl-6 sm:pt-0 sm:w-full md:w-full md:pl-0 md:pt-3">
+              <div className="text-center text-lg font-semibold sm:text-left md:text-center">
+                {song.title}
+              </div>
+              <div
+                className="text-center pt-2 text-sm font-medium sm:text-left md:text-center"
+                style={{ color: "#bdbdbd" }}
+              >
+                {song.description}
+              </div>
             </div>
           </div>
-          <div className="w-2/3 mx-auto">
-            <div className="text-center pt-3 text-lg font-semibold">
-              Two Tone
-            </div>
-            <div
-              className="text-center pt-2 text-sm font-medium"
-              style={{ color: "#bdbdbd" }}
-            >
-              This is my first song off my new project. Let it ride
-            </div>
-          </div>
-        </div>
-        <div className="text-white flex flex-col relative py-4 sm:w-2/5">
-          <div className="flex justify-center relative">
-            <div className="w-full h-full relative aspect-ratio-16/9">
-              <ReactPlayer
-                url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-                className="pin-t pin-l absolute"
-                width="100%"
-                height="100%"
-                controls
-              />
-            </div>
-          </div>
-          <div className="w-2/3 mx-auto">
-            <div className="text-center pt-3 text-lg font-semibold">
-              Two Tone
-            </div>
-            <div
-              className="text-center pt-2 text-sm font-medium"
-              style={{ color: "#bdbdbd" }}
-            >
-              This is my first song off my new project. Let it ride
-            </div>
-          </div>
-        </div>
-        <div className="text-white flex flex-col relative py-4 sm:w-2/5">
-          <div className="flex justify-center relative">
-            <div className="w-full h-full relative aspect-ratio-16/9">
-              <ReactPlayer
-                url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-                className="pin-t pin-l absolute"
-                width="100%"
-                height="100%"
-                controls
-              />
-            </div>
-          </div>
-          <div className="w-2/3 mx-auto">
-            <div className="text-center pt-3 text-lg font-semibold">
-              Two Tone
-            </div>
-            <div
-              className="text-center pt-2 text-sm font-medium"
-              style={{ color: "#bdbdbd" }}
-            >
-              This is my first song off my new project. Let it ride
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   </div>
 );
-
-// src="https://scontent-lax3-1.cdninstagram.com/vp/71c26a5f0b7821b987207ddd032c98c9/5BC0F89F/t51.2885-15/e35/30944464_606228469727505_3986757298922979328_n.jpg"
-// src="https://scontent-lax3-1.cdninstagram.com/vp/22a708445e98a7a912217723cdfe8f62/5BB91B1D/t51.2885-15/e35/29094505_218856788863277_8434260478699503616_n.jpg"
 
 export default MusicSection;
---
+++ src/content/music.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    title: 'Two On',
+    description: 'This is my first song off my new project. Let it ride'
+  },
+  {
+    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    title: 'Two On',
+    description: 'This is my first song off my new project. Let it ride'
+  },
+  {
+    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    title: 'Two On',
+    description: 'This is my first song off my new project. Let it ride'
+  },
+  {
+    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    title: 'Two On',
+    description: 'This is my first song off my new project. Let it ride'
+  },
+  {
+    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    title: 'Two On',
+    description: 'This is my first song off my new project. Let it ride'
+  },
+  {
+    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
+    title: 'Two On',
+    description: 'This is my first song off my new project. Let it ride'
+  }  
+]
