fca56682bae9b6e1f8d1c9bac07bda05f4098a97
Ammon <skatea09@aol.com>
2018-06-19 02:09:05 -0700

    makes react player play one song at a time

--- src/components/MusicSection.jsx
+++ src/components/MusicSection.jsx
@@ -1,48 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactPlayer from "react-player";
 import musicContent from "../content/music.js";
 
-const MusicSection = () => (
-  <div id="music" className="bg-black-true">
-    <div className="sm:w-9/10 mx-auto">
-      <div className="text-white py-2 text-center music-title text-3xl sm:text-left md:text-center">
-        Music
-      </div>
-      <div className="md:flex md:flex-wrap md:justify-between">
-        {musicContent.map(song => (
-          <div className="text-white flex flex-col relative py-4 sm:flex-row md:flex-col md:w-3/10">
-            <div className="flex justify-center relative sm:w-full">
-              <div className="w-full h-full relative aspect-ratio-16/9">
-                <ReactPlayer
-                  url={song.link}
-                  className="pin-t pin-l absolute"
-                  width="100%"
-                  height="100%"
-                  controls
-                  config={{
-                    youtube: {
-                      playerVars: { modestbranding: 0 }
-                    }
-                  }}
-                />
-              </div>
-            </div>
-            <div className="w-2/3 pt-3 mx-auto sm:pl-6 sm:pt-0 sm:w-full md:w-full md:pl-0 md:pt-3">
-              <div className="text-center text-lg font-semibold sm:text-left md:text-center">
-                {song.title}
-              </div>
-              <div
-                className="text-center pt-2 text-sm font-medium sm:text-left md:text-center"
-                style={{ color: "#bdbdbd" }}
-              >
-                {song.description}
-              </div>
-            </div>
+class MusicSection extends Component {
+  state = { playing: null };
+
+  render() {
+    return (
+      <div id="music" className="bg-black-true">
+        <div className="sm:w-9/10 mx-auto">
+          <div className="text-white py-2 text-center music-title text-3xl sm:text-left md:text-center">
+            Music
           </div>
-        ))}
+          <div className="md:flex md:flex-wrap md:justify-between">
+            {musicContent.map(song => (
+              <div className="text-white flex flex-col relative py-4 sm:flex-row md:flex-col md:w-3/10">
+                <div className="flex justify-center relative sm:w-full">
+                  <div className="w-full h-full relative aspect-ratio-16/9">
+                    <ReactPlayer
+                      onPlay={() => this.setState({ playing: song.index })}
+                      onPause={() => this.state.playing === song.index ? this.setState({ playing: null}) : null}
+                      playing={this.state.playing === song.index ? true : false}
+                      url={song.link}
+                      className="pin-t pin-l absolute"
+                      width="100%"
+                      height="100%"
+                      controls
+                      config={{
+                        youtube: {
+                          playerVars: { modestbranding: 0 }
+                        },
+                        soundcloud: {
+                          options: {
+                            show_artwork: true,
+                            show_playcount: false,
+                            show_user: false,
+                            sharing: false
+                          }
+                        }
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className="w-2/3 pt-3 mx-auto sm:pl-6 sm:pt-0 sm:w-full md:w-full md:pl-0 md:pt-3">
+                  <div className="text-center text-lg font-semibold sm:text-left md:text-center">
+                    {song.title}
+                  </div>
+                  <div
+                    className="text-center pt-2 text-sm font-medium sm:text-left md:text-center"
+                    style={{ color: "#bdbdbd" }}
+                  >
+                    {song.description}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default MusicSection;
--- src/content/music.js
+++ src/content/music.js
@@ -2,31 +2,37 @@ export default [
   {
     link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
     title: 'Two On',
-    description: 'This is my first song off my new project. Let it ride'
+    description: 'This is my first song off my new project. Let it ride',
+    index: 0
+  },
+  {
+    link: 'https://soundcloud.com/uiceheidd/lucid-dreams-forget-me',
+    title: 'Switch It Up',
+    description: 'Do things differently sometimes',
+    index: 1
   },
   {
     link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
     title: 'Two On',
-    description: 'This is my first song off my new project. Let it ride'
+    description: 'This is my first song off my new project. Let it ride',
+    index: 2
   },
   {
     link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
     title: 'Two On',
-    description: 'This is my first song off my new project. Let it ride'
+    description: 'This is my first song off my new project. Let it ride',
+    index: 3
   },
   {
     link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
     title: 'Two On',
-    description: 'This is my first song off my new project. Let it ride'
+    description: 'This is my first song off my new project. Let it ride',
+    index: 4
   },
   {
     link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
     title: 'Two On',
-    description: 'This is my first song off my new project. Let it ride'
-  },
-  {
-    link: 'https://www.youtube.com/watch?v=ysz5S6PUM-U',
-    title: 'Two On',
-    description: 'This is my first song off my new project. Let it ride'
-  }  
+    description: 'This is my first song off my new project. Let it ride',
+    index: 5
+  }
 ]
