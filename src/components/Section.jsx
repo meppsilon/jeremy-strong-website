407bcf0de422878f76ec4a608d3060c53227ae99
Ammon <skatea09@aol.com>
2018-06-29 02:14:00 -0700

    adds link to title categories

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,52 +8,58 @@ import RealPlayer from "./Player/RealPlayer";
 class Section extends Component {
   render() {
     const { title, posts, slug, limit } = this.props;
+
     return (
       <div id={title.toLowerCase()}>
         <div className="mx-auto sm:w-9/10">
-          <div className="text-white py-6 music-title text-center sm:text-left text-3xl font-semibold">
+          <Link
+            className="block text-white py-6 music-title text-center sm:text-left text-3xl font-semibold"
+            to={`/${title.toLowerCase()}`}
+          >
             {title}
-          </div>
+          </Link>
           <div className="md:flex md:flex-wrap md:-mx-4">
-            {posts.edges.slice(0, limit).map(
-              (
-                {
-                  node: {
-                    frontmatter: post,
-                    fields: { slug: slug }
-                  }
-                },
-                i
-              ) => (
-                <div
-                  className="text-white flex flex-col relative py-4 sm:flex-row md:flex-col md:w-1/3 md:px-4"
-                  key={`section-post-${post.title}`}
-                >
-                  <div className="w-full">
-                    <DummyPlayer
-                      url={post.link}
-                      dummyClick={() => navigateTo(slug)}
-                    />
-                  </div>
-                  <div className="w-2/3 pt-3 mx-auto sm:pl-6 sm:pt-0 sm:w-full md:w-full md:pl-0 md:pt-3">
-                    <Link
-                      to={slug}
-                      className="block text-center text-lg font-semibold text-white sm:text-left md:text-center"
-                    >
-                      {post.title}
-                    </Link>
-                    <Dotdotdot clamp={3}>
-                      <div
-                        className="text-center pt-2 text-sm font-medium sm:text-left md:text-center"
-                        style={{ color: "#bdbdbd" }}
+            {posts.edges
+              .slice(0, limit)
+              .map(
+                (
+                  {
+                    node: {
+                      frontmatter: post,
+                      fields: { slug: slug }
+                    }
+                  },
+                  i
+                ) => (
+                  <div
+                    className="text-white flex flex-col relative py-4 sm:flex-row md:flex-col md:w-1/3 md:px-4"
+                    key={`section-post-${post.title}`}
+                  >
+                    <div className="w-full">
+                      <DummyPlayer
+                        url={post.link}
+                        dummyClick={() => navigateTo(slug)}
+                      />
+                    </div>
+                    <div className="w-2/3 pt-3 mx-auto sm:pl-6 sm:pt-0 sm:w-full md:w-full md:pl-0 md:pt-3">
+                      <Link
+                        to={slug}
+                        className="block text-center text-lg font-semibold text-white sm:text-left md:text-center"
                       >
-                        {post.description}
-                      </div>
-                    </Dotdotdot>
+                        {post.title}
+                      </Link>
+                      <Dotdotdot clamp={3}>
+                        <div
+                          className="text-center pt-2 text-sm font-medium sm:text-left md:text-center"
+                          style={{ color: "#bdbdbd" }}
+                        >
+                          {post.description}
+                        </div>
+                      </Dotdotdot>
+                    </div>
                   </div>
-                </div>
-              )
-            )}
+                )
+              )}
           </div>
           {posts.totalCount > limit && (
             <Link to={slug} className="flex items-center justify-center py-6">
