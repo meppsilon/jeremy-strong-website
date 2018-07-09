d37f1c2a0cc9d81d839164ac29c3881fc7b8d14a
mepps32 <mepps32@gmail.com>
2018-07-09 15:38:53 -0700

    Fix some bugs on navbar and make divider line grey-light

--- src/components/Navbar/MenuButton.jsx
+++ src/components/Navbar/MenuButton.jsx
@@ -1,17 +1,21 @@
-import React from "react";
+import React from 'react';
 
-const Menu = ({ open, openMenu }) => 
-  open ? (
-    <i
-      className="fa fa-times fa-lg links-social text-white"
-      style={{ paddingTop: "4px" }}
-      onClick={() => openMenu(false)}
-    />  ) : (
-    <i
-      className="fa fa-bars fa-lg links-social text-white"
-      style={{ paddingTop: "4px" }}
-      onClick={() => openMenu(true)}
-    />
-  );
+const Menu = ({ open, openMenu }) => (
+  <div style={{ height: 21, width: 35 }}>
+    {open ? (
+      <i
+        className="fa fa-times fa-lg links-social text-white"
+        style={{ paddingTop: '4px' }}
+        onClick={() => openMenu(false)}
+      />
+    ) : (
+      <i
+        className="fa fa-bars fa-lg links-social text-white"
+        style={{ paddingTop: '4px' }}
+        onClick={() => openMenu(true)}
+      />
+    )}
+  </div>
+);
 
 export default Menu;
