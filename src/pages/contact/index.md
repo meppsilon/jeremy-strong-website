9023dc708226deeca4bbe921ffab45f5403e08ee
mepps32 <mepps32@gmail.com>
2018-08-02 18:11:48 -0700

    Add contact page

---
+++ src/pages/contact/index.md
@@ -0,0 +1,21 @@
+---
+templateKey: "contact-page"
+categories:
+  - contacts:
+      - email: saul@goodvibecrew.com
+        name: Saul Luzeus
+        phone: (310) 944-8080
+    name: Management
+  - contacts:
+      - email: joshua@rcwmediagroup.com
+        name: Joshua Pinkay
+        phone: (504) 274-5086
+      - email: casandra@rcwmediagroup.com
+        name: Casandra Walker
+        phone: (323) 979-8417
+    name: Press/Media Inquiries
+  - contacts:
+      - email: winwinmusic1@gmail.com
+        name: Jay Dnairo
+    name: Record Label
+---
---
+++ src/templates/contact-page.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export const ContactPageTemplate = ({ categories }) => (
+  <div id="contact" className="pt-8">
+    <h1 className="text-white music-title text-center text-4xl font-semibold self-center py-6">Contact</h1>
+    <div className="text-center">
+      {categories.map(({ name, contacts }) => (
+        <div className="text-white" style={{ marginBottom: '2rem' }} key={name}>
+          <div className="text-xl font-bold mb-2">{name}</div>
+          {contacts.map(contact => (
+            <div className="mb-4" key={`${name}-${contact.name}`}>
+              <p className="text-white text-sm font-semibold block mb-1">
+                {contact.name}
+              </p>
+              {contact.email && (
+                <a
+                  className="text-white text-sm font-medium block mb-1"
+                  href={`mailto:${contact.email}`}
+                >
+                  {contact.email}
+                </a>
+              )}
+              {contact.phone && (
+                <p className="text-white block text-sm font-medium">
+                  {contact.phone}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+const ContactPage = ({
+  data: {
+    markdownRemark: {
+      frontmatter: { categories },
+    },
+  },
+}) => <ContactPageTemplate categories={categories} />;
+
+export default ContactPage;
+
+export const contactPageQuery = graphql`
+  query ContactPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        categories {
+          name
+          contacts {
+            name
+            phone
+            email
+          }
+        }
+      }
+    }
+  }
+`;
